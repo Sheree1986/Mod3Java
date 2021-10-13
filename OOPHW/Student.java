dbdc98ab6f38c2679523ea46e543ac3b5c735578
Sheree Edmund <87440131+Sheree1986@users.noreply.github.com>
2021-10-13 05:37:19 -0400

    Update Student.java
    updated person interface second assignment

--- OOPHW/Student.java
+++ OOPHW/Student.java
@@ -1,46 +1,51 @@
 package OOPHW;
 
 public class Student extends Person implements PersonInterface {
-
+//assigning that class 
 
 
 
 		static String school = "Per Scholas";
 		String grade;
 		double css, javascript, java;
+	    int number;
 
 		Student(String name, String address, String phone, String grade, double css, double javascript,
-				double java) {
+				double java, int number) {
+			
 			super(name, address, phone);
 			this.grade = grade;
 			this.css = css;
 			this.javascript = javascript;
 			this.java = java;
+			this.number = number;
+			
 		}
 
 		
-		double calculate() {
-			double sum = this.css + this.javascript + this.java;
-			return sum / 3;
+	
 
-		}
-
-		@Override public void display() {
+		   public void display() {
 			System.out.println("********************");
+			System.out.println(ORGINIZATION);
 			System.out.println("Name of the school the student atttends: " + school);
 			System.out.println("Student's name: " + this.name);
 			System.out.println("Student's address: " + this.address);
 			System.out.println("Student's phone number: " + this.phone);
 			System.out.println("The student is currently a : " + this.grade);
-			System.out.println("The average of the three subjects is: " + calculate());
+			System.out.println("The average of the three subjects is: " + calculate(3));
+			System.out.println("number of donuts you ate today out of a dozen: " + compute(1, 12));
 			System.out.println();
 
 		}
+			public double calculate(int average) {
+				double sum = this.css + this.javascript + this.java;
+				return sum / average;
 
+			}
 
-		@Override
-		public double calculate(int value) {
-			// TODO Auto-generated method stub
-			return 0;
+	    public int compute(int n1, int n2) {
+	    int amount = 12 - this.number;
+	    return amount;
+	    }
 		}
-	}
