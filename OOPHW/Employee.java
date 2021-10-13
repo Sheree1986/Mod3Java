69b36184ad5a91fd8f6ea1bca035ac8e57497d8d
Sheree Edmund <87440131+Sheree1986@users.noreply.github.com>
2021-10-13 05:33:45 -0400

    Update Employee.java
    update the person interface values

--- OOPHW/Employee.java
+++ OOPHW/Employee.java
@@ -1,36 +1,47 @@
 package OOPHW;
 
 
-public class Employee extends Person {
+public class Employee extends Person implements PersonInterface{
 
-	
-
-
-	static String company = "Per Scholar";
-	
 	int jobId;
+	int bonus;
+	int validation;
 	String department;
-	long salary;
+	double salary;
+	
 
-	Employee(String name, String address, String phone, int jobId, String department, long salary) {
+	Employee(String name, String address, String phone, int jobId, int bonus, int validation, String department, double salary) 
+	{
+		
 		super(name, address, phone);
+		
 		this.jobId = jobId;
 		this.department = department;
+		this.bonus = bonus;
+		this.validation = validation;
 		this.salary = salary;
 	}
 
-	void display() {
-		System.out.println();
+	public void display() {
+		System.out.println(ORGINIZATION);
 		System.out.println("***********");
-		System.out.println("Company Name: " + company);
-		System.out.println("Name: " + this.name);
-		System.out.println("Address: " + this.address);
-		System.out.println("Phone: " + this.phone);
+		System.out.println("Student's name: " + this.name);
+		System.out.println("Student's address: " + this.address);
+		System.out.println("Student's phone number: " + this.phone);
 		System.out.println("Job ID Number is: " + this.jobId);
 		System.out.println("department: " + this.department);
-		System.out.println("Salary: " + this.salary);
+		System.out.println("Total Salary: " + this.salary + this.bonus);
+		System.out.println("Validation: " + this.jobId + this.validation);
 		System.out.println();
 
 	}
-
+public double calculate(int bonus) {
+	double netsalary = bonus + this.salary;
+			return netsalary;
+}
+public int compute(int n1, int n2) {
+    int validationSticker = this.jobId + 2021;
+    return validationSticker;
+    //each semester employees must have their ID cards validated.
+}  
 }
